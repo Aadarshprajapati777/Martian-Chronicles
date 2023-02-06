841fe98ca9f30805834bbd2ca4bfb5cd07859c53
Aadarsh <aadarsh9811594152@gmail.com>
2023-02-07 03:20:20 +0530

    image fetching and displaying in sucessfull

--- images/api.py
+++ images/api.py
@@ -22,70 +22,91 @@ from PySide6.QtWidgets import (
     QComboBox,
     QCheckBox,
     QButtonGroup,
-    
 )
+
 
 
 
 
 class Window(QDialog):
     def fetchimage(self):
-      query = self.Query.text()
-      queryvalue = self.QueryValue.text()
-      parameter = self.Parameter.text()
+      query = self.Query.currentText()
+      queryvalue = self.QueryValue.currentText()
       parmvalue = self.Parametervalue.text()
+      api_key = 'XYvgXmCtiXfENZ2bi1d6FRZRYoKEde4GWCJiNaEo'
+      parameter = 'camera'
+
       
 
     #   query='sol'
-    #   queryvalue = 1000
-    #   parameter = 'camera'
-    #   parmvalue = 'fhaz'
-      api_key='XYvgXmCtiXfENZ2bi1d6FRZRYoKEde4GWCJiNaEo'
-
+    #   queryvalue = 1000 
+    #   parmvalue = 'fhaz'QComboBox
       print(query)
       print(queryvalue)
       print(parameter)
       print(parmvalue)
-      print(api_key)
 
-      urls =['https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?'+str(query)+'='+str(queryvalue)+'&'+str(parameter)+'='+str(parmvalue)+'&api_key='+str(api_key)]
+      urls =['https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?'+str(query)+'='+str(parmvalue)+'&'+str(parameter)+'='+str(queryvalue)+'&api_key='+str(api_key)]
       for url in urls:
             response = requests.get(url)
             data = response.json()
             photos = []
-            for photo in data['photos']:
-                img= photo['img_src']
-                
-                filename = img.split('/')[-1]
-                with open(filename, 'wb') as f:
-                    f.write(requests.get(img).content)
-                    photos.append(filename)
-                print(photos)
+            for indx, photo in enumerate(data['photos']):
+                with open (str(indx)+'.png', 'wb') as f:
+                    f.write(requests.get(photo['img_src']).content)      
+            self.viewimage()                    
+
+      print(photos)
 
 
+      self.viewimage()            
 
     def __init__(self):
         super().__init__()
+        layout = QFormLayout()
         self.setWindowTitle("Mars Rover Image Downloader")
         self.resize(500, 500)
-        self.Query = QLineEdit()
-        self.QueryValue = QLineEdit()
+        self.Query = QComboBox()
+        self.QueryValue = QComboBox()
         self.Parameter = QLineEdit()
         self.Parametervalue = QLineEdit()
         self.button = QtWidgets.QPushButton("Fetch Image")
         self.button.clicked.connect(self.fetchimage)
         layout = QFormLayout()
-        layout.addRow("Query", self.Query)
-        layout.addRow("Query Value", self.QueryValue)
-        layout.addRow("Parameter", self.Parameter)
+
+        # layout.addRow("Parameter", self.Parameter)
+        # layout.addRow("Parameter Value", self.Parametervalue)
+        layout.addRow(self.Query)
+        layout.addRow("Select Parameter", self.Query)
         layout.addRow("Parameter Value", self.Parametervalue)
+
+
+        self.Query.addItem("sol")
+        self.Query.addItem("earth_date")
+        layout.addRow("Select Camera", self.QueryValue)
+        self.QueryValue.addItem("fhaz")
+        self.QueryValue.addItem("rhaz")
+
+
+
+
         layout.addRow(self.button)
         self.setLayout(layout)
 
-    def view_image(self):
-        self.image = Image.open(self.filename)
+
+    
+
+    def viewimage(self):
+        self.image = QtWidgets.QLabel(self)
+        self.image.setPixmap(QtGui.QPixmap("/home/adarsh/amfoss_backup/amfoss-tasks/martian-rover/0.png"))
+        self.image.setGeometry(0, 0, 500, 500)
         self.image.show()
 
+        print("image shown")
+        layout = QFormLayout()
+    
+      
+      
 
     
     
